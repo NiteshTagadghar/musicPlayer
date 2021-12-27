5e55492555cdc052293446de83901304f64e9678
Nitesh <niteshtagadghar3@gmail.com>
2021-12-27 23:02:52 +0530

    initial commit

---
+++ src/Constants/audio.js
@@ -0,0 +1,27 @@
+import Audio1 from "../assets/audio1.mp3"
+import Audio2 from "../assets/audio2.mp3"
+import Audio3 from "../assets/audio3.mp3"
+import Audio4 from "../assets/audio4.mp3"
+import ImageOne from "../assets/image1.jpg"
+import ImageTwo from "../assets/image2.jpg"
+import ImageThree from "../assets/image3.jpg"
+import ImageFour from "../assets/image4.jpg"
+
+export const audioConstants = {
+    AUD_1: Audio1,
+    AUD_2: Audio2,
+    AUD_3: Audio3,
+    AUD_4: Audio4,
+
+}
+
+
+export const imageConstants = {
+    IMAGE_1: ImageOne,
+    IMAGE_2: ImageTwo,
+    IMAGE_3: ImageThree,
+    IMAGE_4: ImageFour
+}
+
+
+
